38171fb74248f8c6748619e4c607ebeeee04016f
hanhan9334 <lhtony9334@126.com>
2020-11-07 12:42:08 -0500

    db configed

---
+++ server/config/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.mongoDB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
