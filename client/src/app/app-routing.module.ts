2c58d9d38c6aea198fd2093d6ea9a7a1a51a60bb
John Shin <john.shin53@gmail.com>
2020-11-10 20:17:19 -0500

    deleted unnessary pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { SurveyComponent } from './pages/survey/survey.component';
+
 import { SurveyListComponent } from './survey-list/survey-list.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, data: {title: 'Home'}},
-  {path: 'about', component: AboutComponent, data: {title: 'About'}},
-  {path: 'landing', component: LandingComponent, data: {title: 'Welcome'}},
-  {path: 'survey', component: SurveyComponent, data: {title: 'Survey'}},
+
   {path: 'survey-list', component: SurveyListComponent, data: {title: 'Survey List'}},
-  {path: '', redirectTo: '/landing', pathMatch: 'full', data: {title: 'Welcome'}}
+  {path: '', redirectTo: '/survey-list', pathMatch: 'full', data: {title: 'Welcome'}}
 
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { SurveyListModule } from './survey-list/survey-list.module';
 
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { SurveyComponent } from './pages/survey/survey.component';
 import { BasePageComponent } from './partials/base-page/base-page.component';
 
 
@@ -19,10 +15,6 @@ import { BasePageComponent } from './partials/base-page/base-page.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    LandingComponent,
-    SurveyComponent,
     BasePageComponent,
    
   ],
