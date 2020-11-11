b96e73d9a733de43453cfef6bd0fd593fe47f15c
John Shin <john.shin53@gmail.com>
2020-11-10 21:03:06 -0500

    per page

--- client/src/app/survey-list/survey-list.component.ts
+++ client/src/app/survey-list/survey-list.component.ts
@@ -9,12 +9,17 @@ import { SurveyRepository } from './../model/survey.repository';
 export class SurveyListComponent 
 {
   public selectedName = null;
+  public surveyPerPage = 4;
+  public selectedPage = 1;
+
 
   constructor(private repository: SurveyRepository) { }
 
   get surveys(): Survey[]
   {
-    return this.repository.getSurveys(this.selectedName);
+    const pageIndex = (this.selectedPage -1) * this.surveyPerPage;
+    return this.repository.getSurveys(this.selectedName)
+    .slice(pageIndex, pageIndex + this.surveyPerPage);
   }
   get names(): string[]
   {
@@ -22,5 +27,21 @@ export class SurveyListComponent
   }
   changeName(newName?: string): void{
     this.selectedName = newName;
+  }
+  changePage(newPage: number): void
+  {
+    this.selectedPage = newPage;
+  }
+  changePageSize(newSize: number): void 
+  {
+    this.surveyPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[]
+  {
+    return Array(Math.ceil(this.repository
+      .getSurveys(this.selectedName).length / this.surveyPerPage))
+      .fill(0).map((x, i) => i + 1);
   }
 }
